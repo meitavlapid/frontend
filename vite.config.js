da7cbe5ae2b71f4d3880e8ba996fd864d6f8a920
meitavlapid <meitavlapid@gmail.com>
2025-02-22 01:26:00 +0200

    565656

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
+  preview: {
+    port: process.env.PORT || 10000, // שימוש בפורט ש-Render מקצה
+    host: "0.0.0.0", // מאזין לכל החיבורים
+  },
+  server: {
+    port: process.env.PORT || 3000, // גם בסביבת פיתוח
+    host: "0.0.0.0",
   },
 });
